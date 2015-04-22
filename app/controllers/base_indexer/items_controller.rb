3bf35890ba66830647a4e33b173b51b7eeb25fef
Ahmed AlSum <aalsum@stanford.edu>
2015-04-22 12:17:43 -0700

    Indexer logs their error before sending to the caller

--- app/controllers/base_indexer/items_controller.rb
+++ app/controllers/base_indexer/items_controller.rb
@@ -7,14 +7,17 @@ module BaseIndexer
     def new
       begin  
         druid = params[:druid]
+        Rails.logger.debug "Receiving indexing #{druid}"
         targets = params[:subtargets]
         
         indexer = BaseIndexer.indexer_class.constantize.new
         indexer.index druid, targets
         @status = report_success
         render status: 200
+        Rails.logger.debug "Completing indexing #{druid}"
       rescue Exception => e  
         @status = report_failure request.method_symbol, params, e
+        Rails.logger.error @status
         render status: 202
       end
     end
@@ -22,12 +25,16 @@ module BaseIndexer
     def destroy
       begin
         druid = params[:druid]
+        Rails.logger.debug "Receiving deleting #{druid}"
+        
         indexer = BaseIndexer.indexer_class.constantize.new
         indexer.delete druid
         @status= report_success
         render status: 200
+        Rails.logger.debug "Completing deleting #{druid}"
       rescue Exception => e  
         @status =  report_failure request.method_symbol, params, e
+        Rails.logger.error @status
         render status: 202
       end
     end
