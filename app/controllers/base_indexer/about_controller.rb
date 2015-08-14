cf4ad779867b403fe0a9ee67b086a7b76f9df24b
Ahmed AlSum <aalsum@stanford.edu>
2015-08-14 10:57:35 -0700

    Limit the gem specs list to HTML format only

--- app/controllers/base_indexer/about_controller.rb
+++ app/controllers/base_indexer/about_controller.rb
@@ -9,7 +9,7 @@ module BaseIndexer
       
       @result={:app_name=>Rails.configuration.app_name,:rails_env=>Rails.env,:version=>Rails.configuration.app_version,:last_restart=>(File.exists?('tmp/restart.txt') ? File.new('tmp/restart.txt').mtime : "n/a"),:last_deploy=>(File.exists?('REVISION') ? File.new('REVISION').mtime : "n/a"),:revision=>(File.exists?('REVISION') ? File.read('REVISION') : "n/a")}
       @result.update({:solr_cores=>BaseIndexer.solr_configuration_class_name.constantize.instance.get_configuration_hash})
-      @result.update({:gems=>Gem.loaded_specs})
+      @result.update({:gems=>Gem.loaded_specs}) if request.format.html?
         
       respond_to do |format|
         format.json {render json: @result.to_json}
