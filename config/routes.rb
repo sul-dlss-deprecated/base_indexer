3916abb32be815e203e97b595d00855eefcbbe02
Darren Hardy <drh@stanford.edu>
2016-08-25 14:29:37 -0700

    refactor monitoring with OkComputer to replace AboutController and IsItWorking

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 BaseIndexer::Engine.routes.draw do
-  root 'about#index'
-  get 'about/version' => 'about#version'
-  get 'about' => 'about#version'
-
   resources :items, only: [:destroy], param: :druid, default: { format: :json } do
     member do
       patch 'subtargets/:subtarget', action: :update
