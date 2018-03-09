012347b7c66c749b46a48710463398b31672490a
Chris Beer <cabeer@stanford.edu>
2018-03-09 07:39:57 -0800

    Remove unneeded Client.process abstraction

--- lib/base_indexer/solr/client.rb
+++ lib/base_indexer/solr/client.rb
@@ -6,6 +6,12 @@ module BaseIndexer
     # Processes adds and deletes to the solr core
     class Client
       include DiscoveryIndexer::Logging
+      
+      def self.logging_handler(id)
+        proc do |exception, attempt_number, _total_delay|
+          DiscoveryIndexer::Logging.logger.error "#{exception.class} on attempt #{attempt_number} for #{id}"
+        end
+      end
 
       # Add the document to solr, retry if an error occurs.
       # See https://github.com/ooyala/retries for docs on with_retries.
@@ -14,7 +20,19 @@ module BaseIndexer
       # @param solr_connector [RSolr::Client]  is an open connection with the solr core
       # @param max_retries [Integer] the maximum number of tries before fail
       def self.add(id, solr_doc, solr_connector, max_retries = 10)
-        process(id, solr_doc, solr_connector, max_retries, false)
+        with_retries(max_tries: max_retries, handler: logging_handler(id), base_sleep_seconds: 1, max_sleep_seconds: 5) do |attempt|
+          DiscoveryIndexer::Logging.logger.debug "Attempt #{attempt} for #{id}"
+
+          if allow_update?(solr_connector) && doc_exists?(id, solr_connector)
+            update_solr_doc(id, solr_doc, solr_connector)
+            DiscoveryIndexer::Logging.logger.info "Updating #{id} on attempt #{attempt}"
+          else
+            solr_connector.add(solr_doc, :add_attributes => {:commitWithin => 10000})
+            DiscoveryIndexer::Logging.logger.info "Indexing #{id} on attempt #{attempt}"
+          end
+          #solr_connector.commit
+          DiscoveryIndexer::Logging.logger.info "Completing #{id} successfully on attempt #{attempt}"
+        end
       end
 
       # Add the document to solr, retry if an error occurs.
@@ -23,35 +41,11 @@ module BaseIndexer
       # @param solr_connector[RSolr::Client]  is an open connection with the solr core
       # @param max_retries [Integer] the maximum number of tries before fail
       def self.delete(id, solr_connector, max_retries = 10)
-        process(id, {}, solr_connector, max_retries, true)
-      end
-
-      # It's an internal method that receives all the requests and deal with
-      # SOLR core. This method can call add, delete, or update
-      #
-      # @param id [String] the document id, usually it will be druid.
-      # @param solr_doc [Hash] is the solr doc in hash format
-      # @param solr_connector [RSolr::Client]  is an open connection with the solr core
-      # @param max_retries [Integer] the maximum number of tries before fail
-      def self.process(id, solr_doc, solr_connector, max_retries, is_delete = false)
-        handler = proc do |exception, attempt_number, _total_delay|
-          DiscoveryIndexer::Logging.logger.error "#{exception.class} on attempt #{attempt_number} for #{id}"
-        end
-
-        with_retries(max_tries: max_retries, handler: handler, base_sleep_seconds: 1, max_sleep_seconds: 5) do |attempt|
+        with_retries(max_tries: max_retries, handler: logging_handler(id), base_sleep_seconds: 1, max_sleep_seconds: 5) do |attempt|
           DiscoveryIndexer::Logging.logger.debug "Attempt #{attempt} for #{id}"
 
-          if is_delete
-            solr_connector.delete_by_id(id, :add_attributes => {:commitWithin => 10000})
-            DiscoveryIndexer::Logging.logger.info "Deleting #{id} on attempt #{attempt}"
-          elsif allow_update?(solr_connector) && doc_exists?(id, solr_connector)
-            update_solr_doc(id, solr_doc, solr_connector)
-            DiscoveryIndexer::Logging.logger.info "Updating #{id} on attempt #{attempt}"
-          else
-            solr_connector.add(solr_doc, :add_attributes => {:commitWithin => 10000})
-            DiscoveryIndexer::Logging.logger.info "Indexing #{id} on attempt #{attempt}"
-          end
-          #solr_connector.commit
+          solr_connector.delete_by_id(id, :add_attributes => {:commitWithin => 10000})
+          DiscoveryIndexer::Logging.logger.info "Deleting #{id} on attempt #{attempt}"
           DiscoveryIndexer::Logging.logger.info "Completing #{id} successfully on attempt #{attempt}"
         end
       end
--- spec/base_indexer/solr/client_spec.rb
+++ spec/base_indexer/solr/client_spec.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 
 describe BaseIndexer::Solr::Client do
   let(:druid) { 'cb077vs7846' }
-  let (:solr_doc) { double('solr_doc', {}) }
-  let(:max_retries) { 10 }
+  let(:solr_doc) { double('solr_doc', {}) }
+
   describe '.solr_url' do
     it 'should correctly handle urls with both trailing and leading slashes' do
       connectors=[RSolr.connect(url: 'http://localhost:8983/solr/'),RSolr.connect(url: 'http://localhost:8983/solr')]
@@ -13,35 +13,30 @@ describe BaseIndexer::Solr::Client do
     end
   end
 
-  describe '.process' do
-    describe 'delete' do
-      let (:solr_connector) { double('connector', { options: { url: 'http://localhost:8983/solr/' } } ) }
-      let(:is_delete) { true }
-      it 'should send a delete_by_id command' do
-        expect(solr_connector).to receive(:delete_by_id).with(druid, {:add_attributes=>{:commitWithin=>10000}})
-        BaseIndexer::Solr::Client.process(druid, {}, solr_connector, max_retries, is_delete)
-      end
+  describe 'delete' do
+    let (:solr_connector) { double('connector', { options: { url: 'http://localhost:8983/solr/' } } ) }
+    it 'should send a delete_by_id command' do
+      expect(solr_connector).to receive(:delete_by_id).with(druid, {:add_attributes=>{:commitWithin=>10000}})
+      BaseIndexer::Solr::Client.delete(druid, solr_connector)
     end
-    describe 'update' do
-      let(:is_delete) { false }
-      let(:response) { double({}) }
-      let (:solr_connector) { double('connector', { options: { url: 'http://localhost:8983/solr/', allow_update: true } } ) }
-      it 'should send update_solr_doc' do
-        allow(solr_connector).to receive(:get).with("select", {:params=>{:q=>"id:\"cb077vs7846\""}}).and_return(:response)
-        allow(BaseIndexer::Solr::Client).to receive(:update_solr_doc).with(druid, solr_doc, solr_connector)
-        expect(solr_connector).to receive(:add).with(solr_doc, :add_attributes => {:commitWithin => 10000})
-        BaseIndexer::Solr::Client.process(druid, solr_doc, solr_connector, max_retries, is_delete)
-      end
+  end
+  describe 'update' do
+    let(:response) { double({}) }
+    let (:solr_connector) { double('connector', { options: { url: 'http://localhost:8983/solr/', allow_update: true } } ) }
+    it 'should send update_solr_doc' do
+      allow(solr_connector).to receive(:get).with("select", {:params=>{:q=>"id:\"cb077vs7846\""}}).and_return(:response)
+      allow(BaseIndexer::Solr::Client).to receive(:update_solr_doc).with(druid, solr_doc, solr_connector)
+      expect(solr_connector).to receive(:add).with(solr_doc, :add_attributes => {:commitWithin => 10000})
+      BaseIndexer::Solr::Client.add(druid, solr_doc, solr_connector)
     end
-    describe 'add' do
-      let(:is_delete) { false }
-      let(:response) { double({}) }
-      let (:solr_connector) { double('connector', { options: { url: 'http://localhost:8983/solr/', allow_update: true, commitWithin: '10000'} } ) }
-      it 'should send an add command' do
-        allow(solr_connector).to receive(:get).with("select", {:params=>{:q=>"id:\"cb077vs7846\""}}).and_return(:response)
-        expect(solr_connector).to receive(:add).with(solr_doc, :add_attributes => {:commitWithin => 10000})
-        BaseIndexer::Solr::Client.process(druid, solr_doc, solr_connector, max_retries, is_delete)
-      end
+  end
+  describe 'add' do
+    let(:response) { double({}) }
+    let (:solr_connector) { double('connector', { options: { url: 'http://localhost:8983/solr/', allow_update: true, commitWithin: '10000'} } ) }
+    it 'should send an add command' do
+      allow(solr_connector).to receive(:get).with("select", {:params=>{:q=>"id:\"cb077vs7846\""}}).and_return(:response)
+      expect(solr_connector).to receive(:add).with(solr_doc, :add_attributes => {:commitWithin => 10000})
+      BaseIndexer::Solr::Client.add(druid, solr_doc, solr_connector)
     end
   end
 end
